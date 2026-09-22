a07507387efb1c476024acaeef2091699772e2c8
thanarakok-coder <thanarakok@gmail.com>
2026-09-22 09:43:41 +0700

    Create m2_valCss.js

---
+++ modules/m2_valCss.js
@@ -0,0 +1,13 @@
+window.M2_ValCss = {
+    render: function(container) {
+        container.innerHTML = `
+            <div class="mod-header-block">
+                <div class="mod-title-text">M2: ValCss - ช่วยแนะนำการปรับขนาดยา Valproate</div>
+            </div>
+            <div class="placeholder-box">
+                <h3>กำลังพัฒนาโมดูล ValCss</h3>
+                <p>เตรียมพร้อมรับข้อมูลสูตรคำนวณในขั้นตอนถัดไป</p>
+            </div>
+        `;
+    }
+};
