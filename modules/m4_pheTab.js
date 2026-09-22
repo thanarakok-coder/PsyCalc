f63baed7d3267eaf0ca0c4c9d3385985e540f1f7
thanarakok-coder <thanarakok@gmail.com>
2026-09-22 09:44:21 +0700

    Create m4_pheTab.js

---
+++ modules/m4_pheTab.js
@@ -0,0 +1,13 @@
+window.M4_PheTab = {
+    render: function(container) {
+        container.innerHTML = `
+            <div class="mod-header-block">
+                <div class="mod-title-text">M4: PheTab - ช่วยแนะนำการปรับขนาดยา Phenytoin infatab</div>
+            </div>
+            <div class="placeholder-box">
+                <h3>กำลังพัฒนาโมดูล PheTab</h3>
+                <p>เตรียมพร้อมรับข้อมูลสูตรคำนวณในขั้นตอนถัดไป</p>
+            </div>
+        `;
+    }
+};
